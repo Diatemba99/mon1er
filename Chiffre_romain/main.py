9adb9b8cdfe47c6df9248e4e41c52761eaf9e9b9
Diatemba99 <martin.diatta99@gmail.com>
2021-03-13 00:25:31 +0000

    fixing banner for the program

--- Chiffre_romain/main.py
+++ Chiffre_romain/main.py
@@ -1,10 +1,10 @@
 from art import *
-Art=text2art("PYTHON", font="random") 
+Art=text2art("="*20, font="choice") 
 print(Art)
 
-Art2=text2art("DAFFA NEKH DH", font="bulbhead")  
+Art2=text2art("Roman Cipher", font="lognormvariate")  
 print(Art2)
 
-Art3=text2art("Roman", font="sample")  
+Art3=text2art("="*20, font="choice")
 print(Art3)
 
