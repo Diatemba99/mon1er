58642c3ebc23417a487637363ca3bbf384501e1e
Diatemba99 <martin.diatta99@gmail.com>
2021-03-13 00:01:29 +0000

    adding font family types

--- Chiffre_romain/main.py
+++ Chiffre_romain/main.py
@@ -1,5 +1,10 @@
 from art import *
 Art=text2art("PYTHON", font="random") 
+print(Art)
 
 Art2=text2art("DAFFA NEKH DH", font="bulbhead")  
-print(Art)
+print(Art2)
+
+Art3=text2art("Roman", font="graffiti")  
+print(Art3)
+
