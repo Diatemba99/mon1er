fe27a9c8c8df221897f58963af7247a03af43902
Diatemba99 <martin.diatta99@gmail.com>
2021-03-14 22:53:57 +0000

    add version and name

--- Chiffre_romain/main.py
+++ Chiffre_romain/main.py
@@ -5,7 +5,8 @@ print(border)
 
 Art2=text2art("Roman Cipher", font="lognormvariate")  
 print(Art2)
+print("CODED BY DIATEMBA99")
+print("++++  VERSION 1.0.0 ++++")
 
 border2=text2art("="*20, font="choice")
 print(border2)
-
